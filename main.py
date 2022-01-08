4c3725cd9420360869e08a197f48478c8d9caed4
Jamie <jimmy75792@gmail.com>
2022-01-08 17:24:19 +1300

    add main module

--- main.py
+++ main.py
@@ -8,14 +8,38 @@ from mail_handler import MailHandler
 
 
 
-mail_handler = MailHandler()
+# mail_handler = MailHandler()
 
-mail_handler.login()
-while True:
-    mails = mail_handler.check_inbox()
-    mail_handler.process_unread(mails)
-    # db.monitor_schedule()
-    time.sleep(1)
+# mail_handler.login()
+# while True:
+#     mails = mail_handler.check_inbox()
+#     mail_handler.process_unread(mails)
+#     # db.monitor_schedule()
+#     time.sleep(1)
 
-def login():
+# def login():
     
+
+def main():
+    #if email_list.csv not exist:
+        #create email_list
+    #if schedule.csv not exist:
+        #create schedule.csv 
+    #if database not exist:
+        #create database
+    
+    #create thread tkinter
+
+    #if connect button pushed: 
+        #login (create global token)
+        #create thread mail handler
+    
+        #while is valid token:
+            #refresh token
+            #monitor files
+            #if it's submission deadline:
+                #create thread distribute submission
+
+
+
+ main()
